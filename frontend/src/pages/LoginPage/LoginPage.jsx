88131e42fbcab687c65c629c5cc8aa99a876d729
Nareeco <coconareeco@gmail.com>
2025-12-12 16:44:17 +1100

    Login and Signup page CSS (unfinished)

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom"
 import { useState } from "react";
+import './LoginPage.css'
 import facebook from '../../assets/images/facebook.png';
 import instagram from '../../assets/images/instagram.png';
 import twitter from '../../assets/images/twitter.png';
@@ -13,30 +14,30 @@ const LoginPage = () => {
 
     return (
       <div className = "login-page">
-        <div className = "left-pane" />
+        <div className = "login-left-pane" />
 
-        <div className = "center-pane">
-          <h1 className = "title">Login</h1>
+        <div className = "login-center-pane">
+          <h1 className = "login-title">Login</h1>
 
-          <div className="social-row">
+          <div className="login-social-row">
             <img src={facebook} alt="Facebook Logo" className="facebook-logo" />
             <img src={instagram} alt="Instagram Logo" className="instagram-logo" />
             <img src={twitter} alt="Twitter Logo" className="twitter-logo" />
           </div>
 
-          <div className = "input-group">
+          <div className = "login-input-group">
             <label>Username</label>
             <input
-              className = "text-input"
+              className = "login-text-input"
               placeholder="Enter your username"
               value={username}
               onChange = {(e) => setUsername(e.target.value)}
             />
           </div>
-          <div className = "input-group">
+          <div className = "login-input-group">
             <label>Password</label>
             <input
-              className = "text-input"
+              className = "login-text-input"
               placeholder="Enter your password"
               value={password}
               onChange = {(e) => setPassword(e.target.value)}
@@ -44,10 +45,10 @@ const LoginPage = () => {
           </div>
           <button className = "signin-btn">Login</button>
         </div>
-        <div className = "right-pane">
-          <h2 className = "right-title">Don't have an account yet?</h2>
-          <h1 className = "right-title">Sign Up</h1>
-          <p className = "right-sub">Join us to hate bomb your prof!!</p>
+        <div className = "login-right-pane">
+          <h2 className = "login-right-title">Don't have an account yet?</h2>
+          <h1 className = "login-right-title">Sign Up</h1>
+          <p className = "login-right-sub">Join us to hate bomb your prof!!</p>
 
           <button onClick={() => navigate("/signup")}>Sign Up</button>
         </div>
--- frontend/src/pages/SignupPage/SignupPage.jsx
+++ frontend/src/pages/SignupPage/SignupPage.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom"
 import { useState } from "react";
+import './SignupPage.css'
 import facebook from '../../assets/images/facebook.png';
 import instagram from '../../assets/images/instagram.png';
 import twitter from '../../assets/images/twitter.png';
@@ -14,50 +15,50 @@ const SignupPage = () => {
 
     return (
       <div className = "signup-page">
-        <div className = "left-pane">
-          <h1 className = "left-title">Welcome Back</h1>
+        <div className = "signup-left-pane">
+          <h1 className = "signup-left-title">Welcome Back</h1>
 
           <button onClick={() => navigate("/login")}>Sign In</button>
         </div>
-        <div className = "right-pane" />
+        <div className = "signup-right-pane" />
 
-        <div className = "center-pane">
-          <h1 className = "title">Sign Up</h1>
+        <div className = "signup-center-pane">
+          <h1 className = "signup-title">Sign Up</h1>
 
-          <div className="social-row">
+          <div className="signup-social-row">
             <img src={facebook} alt="Facebook Logo" className="facebook-logo" />
             <img src={instagram} alt="Instagram Logo" className="instagram-logo" />
             <img src={twitter} alt="Twitter Logo" className="twitter-logo" />
           </div>
 
-          <div className = "input-group">
+          <div className = "signup-input-group">
             <label>Username</label>
             <input
-              className = "text-input"
+              className = "signup-text-input"
               placeholder="Enter your username"
               value={username}
               onChange = {(e) => setUsername(e.target.value)}
             />
           </div>
-          <div className = "input-group">
+          <div className = "signup-input-group">
             <label>Email</label>
             <input
-              className = "text-input"
+              className = "signup-text-input"
               placeholder="Enter your email"
               value={email}
               onChange = {(e) => setEmail(e.target.value)}
             />
           </div>
-          <div className = "input-group">
+          <div className = "signup-input-group">
             <label>Password</label>
             <input
-              className = "text-input"
+              className = "signup-text-input"
               placeholder="Enter your password"
               value={password}
               onChange = {(e) => setPassword(e.target.value)}
             />
           </div>
-          <button className = "signin-btn">Sign up</button>
+          <button className = "signup-btn">Sign up</button>
         </div>
       </div>
     )
