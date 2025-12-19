3b2d62844674c99d156e3a917cfcb081c3b2fae2
Sam <z5588327@ad.unsw.edu.au>
2025-12-20 00:50:48 +1100

    add sidebar and create tutor profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import './App.css'
+import CreateProfile from "./pages/CreateProfile/CreateProfile";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/create-profile" element={<CreateProfile />} />
       </Routes>
     </BrowserRouter>
   )
